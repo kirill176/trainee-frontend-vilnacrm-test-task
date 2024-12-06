8c2440066f53915b3747f5181e5d164f62044423
Kyryl Siedykh <sdsbezoht123@gmail.com>
2024-12-06 15:04:39 +0200

    fix some workfow bugs

--- src/services/api.ts
+++ src/services/api.ts
@@ -12,8 +12,14 @@ const api = axios.create({
 });
 
 export const fetchData = async () => {
+  const cachedData = localStorage.getItem('userData');
+  if (cachedData) {
+    return JSON.parse(cachedData);
+  }
+
   try {
     const response = await api.get('/users/1');
+    localStorage.setItem('userData', JSON.stringify(response.data));
     return response.data;
   } catch (error) {
     console.error('Error while receiving data:', error);
